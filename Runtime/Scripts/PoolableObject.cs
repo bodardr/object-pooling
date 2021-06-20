42046ef4e0e633482abd98918b5fb4fba2e957a9
Roman B <romain.bodard@videotron.ca>
2021-06-19 21:41:59 -0400

    Implemented basic object pooling, SOPool and prefab pool
    Signed-off-by: Roman B <romain.bodard@videotron.ca>

--- Runtime/Scripts/PoolableObject.cs
+++ Runtime/Scripts/PoolableObject.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public struct PoolableObject<T> : IDisposable
+public class PoolableObject<T>
 {
     private IObjectPool<T> pool;
-    public T Content { get; }
-
-    public void Dispose()
-    {
-        if (pool.Contains(this))
-            Debug.Log("Object already disposed.");
-        
-        pool.Dispose(this);
-    }
 
     public PoolableObject(T content, IObjectPool<T> pool)
     {
         Content = content;
         this.pool = pool;
+    }
+
+    public T Content { get; }
+
+    public Scene ReferencedScene { get; set; }
+
+    public virtual void Release()
+    {
+        if (pool.Contains(this))
+            Debug.Log("Object already disposed.");
+
+        pool.Retrieve(this);
     }
 }
---
+++ Runtime/Scripts/ScriptableObjectPool.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Prefab Pool", menuName = "Object Pool/Prefab")]
+public class ScriptableObjectPool<T> : ScriptableObject, IObjectPool<T> where T : Object
+{
+    [SerializeField]
+    protected GameObject prefab;
+
+    [SerializeField]
+    protected int baseInstantiation = 10;
+
+    private Stack<PoolableObject<T>> pool;
+
+    protected void OnEnable()
+    {
+        SceneManager.sceneUnloaded += OnSceneChanged;
+        InstantiatePool();
+    }
+
+
+    protected void OnDisable()
+    {
+        SceneManager.sceneUnloaded -= OnSceneChanged;
+        DestroyPool();
+    }
+
+    public virtual void Retrieve(PoolableObject<T> poolable) => pool.Push(poolable);
+
+    public bool Contains(PoolableObject<T> poolable) => pool.Contains(poolable);
+
+    private void OnSceneChanged(Scene unloadedScene)
+    {
+        if (pool == null)
+            return;
+
+        foreach (var element in pool.Where(element => element.ReferencedScene == unloadedScene))
+            element.Release();
+    }
+
+    public PoolableObject<T> Get()
+    {
+        if (pool == null)
+            InstantiatePool();
+
+        if (pool.Count == 0)
+        {
+            Debug.Log("Pool base exceeded, consider increasing the pool.");
+            return InstantiateSingleObject();
+        }
+
+        return pool.Pop();
+    }
+
+    protected virtual void InstantiatePool()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        pool = new Stack<PoolableObject<T>>();
+
+        for (int i = 0; i < baseInstantiation; i++)
+        {
+            pool.Push(InstantiateSingleObject(i));
+        }
+    }
+
+    protected virtual PoolableObject<T> InstantiateSingleObject(int index = 0)
+    {
+        var go = Instantiate(prefab);
+        go.name = $"{prefab.name}{(index > 0 ? $" {index}" : "")} (Pooled)";
+        go.SetActive(false);
+        DontDestroyOnLoad(go);
+
+        return new PoolableObject<T>(go.GetComponent<T>(), this);
+    }
+
+    protected virtual void DestroyPool()
+    {
+        if (pool == null)
+            return;
+
+        while (pool.Count > 0)
+        {
+            var element = pool.Pop();
+
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                DestroyImmediate(element.Content);
+#else
+            Destroy(element.Content);
+#endif
+        }
+
+        pool = null;
+    }
+}
